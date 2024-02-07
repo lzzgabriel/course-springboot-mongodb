5e2f2612c514ecf7a8696139a6144c783b8ce515
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2024-02-07 09:56:23 -0300

    :tada: Primeiro commit

---
+++ src/main/java/dev/lzzgabriel/coursespringbootmongodb/CourseSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package dev.lzzgabriel.coursespringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lzzgabriel/coursespringbootmongodb/CourseSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lzzgabriel.coursespringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
