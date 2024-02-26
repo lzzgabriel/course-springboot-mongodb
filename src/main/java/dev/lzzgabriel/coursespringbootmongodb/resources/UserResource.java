65454cde8e1a9cdb2e5383c7da9fd1c746c65147
lzzgabriel <gabrielhlezo@gmail.com>
2024-02-26 16:05:36 -0300

    Deleção de usuário com delete

--- src/main/java/dev/lzzgabriel/coursespringbootmongodb/resources/UserResource.java
+++ src/main/java/dev/lzzgabriel/coursespringbootmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,12 +40,18 @@ public class UserResource {
     return ResponseEntity.ok(new UserDTO(user));
   }
   
-  @PostMapping(value = "")
+  @PostMapping
   public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
     var obj = service.fromDTO(objDTO);
     obj = service.insert(obj);
     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
     return ResponseEntity.created(uri).build();
+  }
+  
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable String id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
   }
   
 
